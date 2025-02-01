16ba3a346be3884b382b9c95ae83289b80f3b31b
Stela Nascimento <stelaoliver2020@gmail.com>
2025-02-01 20:29:26 -0300

    Banco de Dados Feito

--- query.sql
+++ query.sql
@@ -7,6 +7,22 @@ INSERT INTO tarefas (titulo, descricao, status, data_vencimento)
 VALUES ($1, $2, $3, $4)
 RETURNING id, titulo, descricao, status, data_vencimento;
 
+UPDATE tarefas
+SET titulo = $1,
+	descricao = $2,
+    status = $3,
+    data_vencimento = $4
+WHERE id = $5
+RETURNING id, titulo, descricao, status, data_vencimentol;
+
+DELETE FROM tarefas
+WHERE id = $1
+RETURNING id;
+
+SELECT id, titulo, descricao, status, data_vencimento
+FROM tarefas
+WHERE status = $1;
+
 
 
 
